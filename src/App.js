e1226f9ef8f1623d619f057f32bd57c1b4e33ad7
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-07-15 10:28:03 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+import "./App.css"
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    state = {
+        alunos: [
+            {
+                nome: "David",
+                idade: 28,
+                tecnologia: "HTML"
+            }, 
+            {
+                nome: "Mariana",
+                idade: 18,
+                tecnologia: "CSS"
+            }, 
+            {
+                nome: "Rômulo",
+                idade: 29,
+                tecnologia: "JavaScript"
+            },
+            {
+                nome: "Fernanda",
+                idade: 31,
+                tecnologia: "React"
+            }
+        ]
+    }
+
+    render(){
+        return(
+            <nav>
+                {/* <ul>
+                    <li>{this.state.alunos[0].nome}</li>
+                    <li>{this.state.alunos[0].idade}</li>
+                    <li>{this.state.alunos[0].tecnologia}</li>
+                </ul>
+                <ul>
+                    <li>{this.state.alunos[1].nome}</li>
+                    <li>{this.state.alunos[1].idade}</li>
+                    <li>{this.state.alunos[1].tecnologia}</li>
+                </ul>
+                <ul>
+                    <li>{this.state.alunos[2].nome}</li>
+                    <li>{this.state.alunos[2].idade}</li>
+                    <li>{this.state.alunos[2].tecnologia}</li>
+                </ul> */}
+                {this.state.alunos.map((item) => (
+                    <div className="conteiner-ul">
+                        <ul className="map-list">
+                            <li>{item.nome}</li>
+                            <li>{item.idade}</li>
+                            <li>{item.tecnologia}</li>
+                        </ul>
+                        <button>Sobre</button>
+                    </div>
+                ))}
+                {/* Callback => Função dentro de um map
+                    () => {}    => Arrow Function */}
+            </nav>
+        )
+    }
 }
 
 export default App;
